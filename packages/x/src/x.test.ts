8320060f79c1239b219de16ca9766527ab1494bc
Chris <full-stack.dev@strolia-davis.us>
2024-03-28 20:33:17 -0400

    #35 Add some initial functionality
    - unit tests
    - initialization script
    - simple core config and response

--- packages/x/src/x.test.ts
+++ packages/x/src/x.test.ts
@@ -3,8 +3,17 @@ import { describe, expect, it } from 'vitest';
 import { x$ } from '.';
 
 describe('x$', () => {
-  it('does stuff', () => {
-    expect(x$).toEqual(x$);
-    expect(x$()).toEqual(x$());
+  it('must be initialized before it will work', () => {
+    expect(() => x$()).toThrow('x$ must be initialized before it can be used');
+    x$.initialize();
+    expect(() => x$()).not.toThrow();
+  });
+
+  it('returns one or more methods based on configuration', () => {
+    const { testFn } = x$({
+      testFn: {},
+    });
+
+    expect(testFn).toBeTypeOf('function');
   });
 });
--- packages/x/src/x.ts
+++ packages/x/src/x.ts
@@ -1,6 +1,30 @@
-export type X$ = () => string;
+export type X$Config = Record<string, {}>;
+export type X$Fn = (config?: X$Config) => Record<string, Function>;
+export type X$ = X$Fn & { initialize: Function };
 
-const x$: X$ = () => 'x$';
+type X$tate = {
+  initialized?: boolean;
+};
+
+const state:X$tate = {};
+
+const x$: X$ = (config= {}) => {
+  if(!state.initialized) {
+    throw Error('x$ must be initialized before it can be used')
+  }
+
+  return Object
+    .entries(config)
+    .reduce(
+      (fns, [key]) => ({...fns, [key]: () => {} }),
+      {} as Record<string, Function>,
+      );
+};
+
+x$.initialize = () => {
+  state.initialized = true;
+};
+
 
 export default x$;
 
